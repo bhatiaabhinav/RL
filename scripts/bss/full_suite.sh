ac5565a38d28aeadf5312945c247bdf672f86ce7
Abhinav Bhatia <bhatiaabhinav93@gmail.com>
2018-11-08 18:52:49 +0800

    useful shell scripts

---
+++ scripts/bss/full_suite.sh
@@ -0,0 +1,6 @@
+sh variants/sddpg_rmsac.sh $1 &
+sh variants/sddpg_logboth.sh $1 &
+sh variants/cddpg_rmsac.sh $1 &
+sh variants/cddpg_logboth.sh $1 &
+sh variants/cddpg_rmsac_wolpert.sh $1 &
+sh variants/cddpg_logboth_wolpert.sh $1 &
---
+++ scripts/ers/greedy.sh
@@ -0,0 +1,3 @@
+$GYM_PYTHON -m RL.reco_rl.greedy_static \
+	--env=$1 \
+	--num_cpu=32
