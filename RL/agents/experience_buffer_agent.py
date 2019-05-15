ebe9a2335db948635bc7905b78e4c52415f9e361
Abhinav Bhatia <bhatiaabhinav93@gmail.com>
2019-05-16 07:57:55 +0800

    imp bug fix abt "ignore done on timelimit"

--- RL/agents/experience_buffer_agent.py
+++ RL/agents/experience_buffer_agent.py
@@ -22,8 +22,8 @@ class ExperienceBufferAgent(RL.Agent):
             self.experience_buffer = ExperienceBuffer(length=self.buffer_length)
         self.nstep_buffers = [[]] * self.context.num_envs  # type: List[Experience]
         self.ignore_done_on_timelimit = False
-        if hasattr(self.context.env, 'max_episode_steps') and override_ignore_done_on_timelimit is None:
-            RL.logger.info("{0}: This is a timelimit environment. Done signal will be ignored if either TimeLimit.truncated info is provided or if not provided, then if length of episode is same as env.max_episode_steps".format(self.name))
+        if hasattr(self.context.env.spec, 'max_episode_steps') and override_ignore_done_on_timelimit is None:
+            RL.logger.info("{0}: This is a timelimit environment. Done signal will be ignored if either TimeLimit.truncated info is provided or if not provided, then if length of episode is same as env.spec.max_episode_steps".format(self.name))
             self.ignore_done_on_timelimit = True
         if override_ignore_done_on_timelimit is not None:
             self.ignore_done_on_timelimit = override_ignore_done_on_timelimit
