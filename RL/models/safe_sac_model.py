1fc535f6b28dc934f6f2940890d3e5d779c8391f
Abhinav Bhatia <bhatiaabhinav93@gmail.com>
2019-05-22 00:54:58 +0530

    safe sac actor loss according to exp penalty

--- RL/models/safe_sac_model.py
+++ RL/models/safe_sac_model.py
@@ -5,8 +5,9 @@ import tensorflow as tf
 class SafeSACModel(SACModel):
     def tf_actor_loss(self, actor_loss_coeffs, actor_loss_alpha, actor_critics, actor_logpis, name):
         with tf.variable_scope(name):
-            action_feasibility = 1 / (1 + tf.exp(-(actor_critics[-1] - self.context.safety_threshold) / self.context.beta))
-            loss = 0
-            loss = sum([-actor_loss_coeffs[i] * actor_critics[i] for i in range(self.num_critics - 1)]) - actor_loss_alpha * actor_loss_coeffs[-1] * tf.log(action_feasibility + 1e-8) + actor_loss_alpha * actor_logpis
+            violation = actor_critics[-1] - self.context.safety_threshold
+            log_feasibility = -tf.exp(violation / self.context.beta)
+            objective = sum([actor_loss_coeffs[i] * actor_critics[i] for i in range(self.num_critics - 1)]) - actor_loss_alpha * actor_logpis + actor_loss_alpha * actor_loss_coeffs[-1] * log_feasibility
+            loss = -objective
             loss = tf.reduce_mean(loss)
             return loss
