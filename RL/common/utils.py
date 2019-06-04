2d34786d2eb3bfc54afc7e779493a62d1cea9b55
Abhinav Bhatia <bhatiaabhinav93@gmail.com>
2019-06-04 18:39:02 +0800

    ability to specify clip gradient amount

--- RL/common/utils.py
+++ RL/common/utils.py
@@ -648,10 +648,12 @@ def tf_l2_norm(vars, exclusions=['output', 'bias'], name='l2_losses'):
 
 def tf_training_step(loss, vars, optimizer, l2_reg, clip_gradients, name):
     with tf.variable_scope(name):
-        loss = loss + l2_reg * tf_l2_norm(vars)
+        if l2_reg:
+            loss = loss + l2_reg * tf_l2_norm(vars)
         grads = tf.gradients(loss, vars)
         if clip_gradients:
-            grads = [tf.clip_by_value(grad, -1, 1) for grad in grads]
+            clip_gradients = float(clip_gradients)
+            grads = [tf.clip_by_value(grad, -clip_gradients, clip_gradients) for grad in grads]
         sgd_step = optimizer.apply_gradients(zip(grads, vars))
         return sgd_step
 
