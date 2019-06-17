2d8da44afc44bd56f87ed3a5ab165c49f9bab0b0
Abhinav Bhatia <bhatiaabhinav93@gmail.com>
2019-06-17 16:35:52 +0800

    safe sac penalty gradient clipping optional

--- RL/models/safe_sac_model.py
+++ RL/models/safe_sac_model.py
@@ -11,9 +11,12 @@ class SafeSACModel(SACModel):
             c = self.context.safe_sac_penalty_max_grad  # max gradient
             b = self.context.beta
             exp_penalty = -tf.exp(-x / b)
-            linear_penalty = c * x + b * c * (np.log(b * c) - 1)  # expression cx + const to make this function cont from where exp_penalty left off.
-            switch_at = -b * np.log(b * c)  # at this point, grad of exp_penalty is c. and the overall penalty should switch to a linear_penalty
-            penalty = (tf.maximum(0.0, x - switch_at) / (x - switch_at)) * exp_penalty + (tf.minimum(0.0, x - switch_at) / (x - switch_at)) * linear_penalty
+            if c:
+                linear_penalty = c * x + b * c * (np.log(b * c) - 1)  # expression cx + const to make this function cont from where exp_penalty left off.
+                switch_at = -b * np.log(b * c)  # at this point, grad of exp_penalty is c. and the overall penalty should switch to a linear_penalty
+                penalty = (tf.maximum(0.0, x - switch_at) / (x - switch_at)) * exp_penalty + (tf.minimum(0.0, x - switch_at) / (x - switch_at)) * linear_penalty
+            else:
+                penalty = exp_penalty
             log_feasibility = penalty
             objective = sum([actor_loss_coeffs[i] * actor_critics[i] for i in range(self.num_critics - 1)]) - actor_loss_alpha * actor_logpis + actor_loss_alpha * actor_loss_coeffs[-1] * log_feasibility
             loss = -objective
